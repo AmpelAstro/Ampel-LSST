fa2b7f44204e37a29641a72ef12c6235ff06b3fc
Marcus Fenner <mfenner@physik.hu-berlin.de>
2022-03-18 14:11:27 +0100

    Update Tabulators

--- ampel/lsst/view/LSSTT2Tabulator.py
+++ ampel/lsst/view/LSSTT2Tabulator.py
@@ -2,31 +2,39 @@
 # -*- coding: utf-8 -*-
 # File              : Ampel-LSST/ampel/lsst/view/LSSTT2Tabulator.py
 # License           : BSD-3-Clause
-# Author            : mf <mf@physik.hu-berlin.de>
+# Author            : Marcus Fenner <mf@physik.hu-berlin.de>
 # Date              : 25.05.2021
 # Last Modified Date: 13.09.2021
-# Last Modified By  : mf <mf@physik.hu-berlin.de>
+# Last Modified By  : Marcus Fenner <mf@physik.hu-berlin.de>
 
-from typing import Sequence, Optional, List, Any, Tuple, Union
+from typing import Any, List, Sequence, Tuple
+
 from ampel.content.DataPoint import DataPoint
-from ampel.abstract.AbsT2Tabulator import AbsT2Tabulator
 from astropy.table import Table
-from ampel.view.LightCurve import LightCurve
-from ampel.content.T1Document import T1Document
+
+from ampel.abstract.AbsT2Tabulator import AbsT2Tabulator
 
 LSST_BANDPASSES = {
-    'u': "lsstu",
-    'g': "lsstg",
-    'r': "lsstr",
-    'i': "lssti",
-    'z': "lsstz",
-    'y': "lssty",
+    "u": "lsstu",
+    "g": "lsstg",
+    "r": "lsstr",
+    "i": "lssti",
+    "z": "lsstz",
+    "y": "lssty",
 }
+
+
 class LSSTT2Tabulator(AbsT2Tabulator):
     convert2jd: bool = True
     """ """
-    def get_flux_table(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Table:
-        flux, fluxerr, filtername, tai = self.get_values(dps,['psFlux','psFluxErr','filterName','midPointTai'])
+
+    def get_flux_table(
+        self,
+        dps: List[DataPoint],
+    ) -> Table:
+        flux, fluxerr, filtername, tai = self.get_values(
+            dps, ["psFlux", "psFluxErr", "filterName", "midPointTai"]
+        )
         if self.convert2jd:
             tai = self._to_jd(tai)
         filters = list(map(LSST_BANDPASSES.get, filtername))
@@ -39,26 +47,41 @@ class LSSTT2Tabulator(AbsT2Tabulator):
                 "zp": [25] * len(filters),
                 "zpsys": ["ab"] * len(filters),
             },
-            dtype = ('float64', 'float64', 'float64', 'str', 'int64', 'str' )
+            dtype=("float64", "float64", "float64", "str", "int64", "str"),
         )
 
-    def get_pos(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Sequence[Tuple[float, float]]:
-        return tuple(zip(*self.get_values(dps, ['ra','decl'])))
+    def get_pos(self, dps: List[DataPoint]) -> Sequence[Tuple[float, float]]:
+        return tuple(zip(*self.get_values(dps, ["ra", "decl"])))
 
-    def get_jd(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Sequence[float]:
-        return self._to_jd(self.get_values(dps,'midPointTai')[0])
+    def get_jd(self, dps: List[DataPoint]) -> Sequence[float]:
+        return self._to_jd(self.get_values(dps, "midPointTai")[0])
 
     @staticmethod
     def _to_jd(dates: Sequence[Any]) -> Sequence[Any]:
-        return [ date - 2400000.5 for date in dates]
+        return [date + 2400000.5 for date in dates]
+
+    def get_stock_id(self, dps: List[DataPoint]) -> set[int]:
+        return set([el["stock"] for el in dps if "ZTF" in el["tag"]])
+
+    def get_stock_name(self, dps: List[DataPoint]) -> set[int]:
+        return self.get_stock_id(dps)
 
     @staticmethod
-    def get_values(dps: Union[LightCurve, List[DataPoint]], params: Sequence[str]) -> Tuple[Sequence[Any],...]:
-        if isinstance(dps, LightCurve):
-            values = dps.get_ntuples(params)
-            if values and all(values):
-                return tuple(map(list, zip(*values)))
-            else:
-                return tuple([] for _ in params)
+    def get_values(
+        dps: List[DataPoint], params: Sequence[str]
+    ) -> Tuple[Sequence[Any], ...]:
+        if tup := tuple(
+            map(
+                list,
+                zip(
+                    *(
+                        [el["body"][param] for param in params]
+                        for el in dps
+                        if "LSST" in el["tag"]
+                    )
+                ),
+            )
+        ):
+            return tup
         else:
-            return tuple(map(list, zip(*([el['body'][param] for param in params] for el in dps if 'LSST' in el['tag']))))
+            return tuple([[]] * len(params))
--- ampel/lsst/view/ZTFT2Tabulator.py
+++ ampel/lsst/view/ZTFT2Tabulator.py
@@ -2,18 +2,19 @@
 # -*- coding: utf-8 -*-
 # File              : Ampel-LSST/ampel/lsst/view/ZTFT2Tabulator.py
 # License           : BSD-3-Clause
-# Author            : mf <mf@physik.hu-berlin.de>
+# Author            : Marcus Fenner <mf@physik.hu-berlin.de>
 # Date              : 26.05.2021
-# Last Modified Date: 13.09.2021
-# Last Modified By  : mf <mf@physik.hu-berlin.de>
+# Last Modified Date: 15.09.2021
+# Last Modified By  : Marcus Fenner <mf@physik.hu-berlin.de>
 
-from typing import Sequence, Optional, List, Any, Tuple, Union
-from ampel.content.DataPoint import DataPoint
-from ampel.abstract.AbsT2Tabulator import AbsT2Tabulator
-from astropy.table import Table
-from ampel.view.LightCurve import LightCurve
+from typing import Any, List, Sequence, Tuple
+
 import numpy as np
-from ampel.content.T1Document import T1Document
+from ampel.content.DataPoint import DataPoint
+from astropy.table import Table
+
+from ampel.abstract.AbsT2Tabulator import AbsT2Tabulator
+from ampel.ztf.util.ZTFIdMapper import ZTFIdMapper
 
 ZTF_BANDPASSES = {
     1: {"name": "ztfg"},
@@ -21,18 +22,27 @@ ZTF_BANDPASSES = {
     3: {"name": "ztfi"},
 }
 
+signdict = {
+    "0": -1,
+    "f": -1,
+    "1": 1,
+    "t": 1,
+}
+
+
 class ZTFT2Tabulator(AbsT2Tabulator):
-
-
-    def get_flux_table(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Table:
-
-        light_curve = self.to_lightcurve(dps, compound)
-        if ( values := light_curve.get_ntuples(('magpsf','sigmapsf','jd','fid')) ) is None:
-            magpsf, sigmapsf, jd, fids = ([],[],[],[])
-        else:
-            magpsf, sigmapsf, jd, fids = zip(*values)
-        filter_names = [ZTF_BANDPASSES[fid]['name'] for fid in fids]
-        flux = np.asarray([ 10 ** (-((mgpsf) - 25)/2.5) for mgpsf in magpsf])
+    def get_flux_table(
+        self,
+        dps: List[DataPoint],
+    ) -> Table:
+        magpsf, sigmapsf, jd, fids, magzpsci, isdiffpos = self.get_values(
+            dps, ["magpsf", "sigmapsf", "jd", "fid", "magzpsci", "isdiffpos"]
+        )
+        filter_names = [ZTF_BANDPASSES[fid]["name"] for fid in fids]
+        signs = [signdict[el] for el in isdiffpos]
+        flux = signs * np.asarray(
+            [10 ** (-((mgpsf) - 25) / 2.5) for mgpsf in magpsf]
+        )
         fluxerr = np.abs(flux * (-np.asarray(sigmapsf) / 2.5 * np.log(10)))
         return Table(
             {
@@ -40,25 +50,45 @@ class ZTFT2Tabulator(AbsT2Tabulator):
                 "flux": flux,
                 "fluxerr": fluxerr,
                 "band": filter_names,
-                "zp": [25] * len(filter_names),
+                "zp": magzpsci,
                 "zpsys": ["ab"] * len(filter_names),
             },
-            dtype = ('float64', 'float64', 'float64', 'str', 'int64', 'str' )
+            dtype=("float64", "float64", "float64", "str", "int64", "str"),
         )
 
-    def get_pos(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Sequence[Tuple[Any, Any]]:
-        return self.to_lightcurve(dps, compound).get_pos('raw')
+    def get_pos(self, dps: List[DataPoint]) -> Sequence[Tuple[float, float]]:
+        return tuple(zip(*self.get_values(dps, ["ra", "dec"])))
 
-    def get_jd(self, dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> Sequence[Any]:
-        if result := self.to_lightcurve(dps, compound).get_values('jd'):
-            return result
-        else:
-            return []
+    def get_jd(
+        self,
+        dps: List[DataPoint],
+    ) -> Sequence[Any]:
+        return self.get_values(dps, "jd")[0]
+
+    def get_stock_id(self, dps: List[DataPoint]) -> set[int]:
+        return set([el["stock"] for el in dps if "ZTF" in el["tag"]])
+
+    def get_stock_name(self, dps: List[DataPoint]) -> set[str]:
+        return set(
+            [ZTFIdMapper.to_ext_id(el) for el in self.get_stock_id(dps)]
+        )
 
     @staticmethod
-    def to_lightcurve(dps: Union[LightCurve, List[DataPoint]], compound: Optional[T1Document] = None) -> LightCurve:
-        if isinstance(dps, LightCurve):
-            return dps
+    def get_values(
+        dps: List[DataPoint], params: Sequence[str]
+    ) -> Tuple[Sequence[Any], ...]:
+        if tup := tuple(
+            map(
+                list,
+                zip(
+                    *(
+                        [el["body"][param] for param in params]
+                        for el in dps
+                        if "ZTF" in el["tag"]
+                    )
+                ),
+            )
+        ):
+            return tup
         else:
-            assert compound is not None
-            return LightCurve.build(compound=compound, datapoints=[dp for dp in dps if 'ZTF' in dp['tag']])
+            return tuple([[]] * len(params))
